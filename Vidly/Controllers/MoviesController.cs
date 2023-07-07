2b8be68db20dd54424f18b1ee188f906807a6241
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-07-07 12:19:12 +0200

    Project files added

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            
+            return View(movie);
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
