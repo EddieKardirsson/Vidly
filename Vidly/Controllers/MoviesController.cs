cfbbb4b39f53df67c3bb61b9059fcdde4173fc76
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-10-19 06:07:39 +0200

    Basic MVC structure completed

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,14 +11,28 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-E"}
+            };
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Customer {Name = "John Smith"},
+                new Customer {Name = "Mary Williams"}
             };
 
             var viewModel = new RandomMovieViewModel
@@ -31,31 +45,31 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
 
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+        //// movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
 
-            if(String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "name";
-            }
+        //    if(String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "name";
+        //    }
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        //}
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "-" + month);
-        }
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "-" + month);
+        //}
     }
 }
