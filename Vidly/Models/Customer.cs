751d9c2ea6a4dd5fb51b43a3bdeb27754da848bd
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-10-19 08:26:45 +0200

    Database setup with Entity Framework. Initial Code-first migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,15 +9,15 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string Phone { get; set; }
-        public string Fax { get; set; }
-        public string HomePage { get; set; }
-        public string Extension { get; set; }
-        public string PhoneNumber { get; set; }
+        //public string Address { get; set; }
+        //public string City { get; set; }
+        //public string Region { get; set; }
+        //public string PostalCode { get; set; }
+        //public string Country { get; set; }
+        //public string Phone { get; set; }
+        //public string Fax { get; set; }
+        //public string HomePage { get; set; }
+        //public string Extension { get; set; }
+        //public string PhoneNumber { get; set; }
     }
 }
